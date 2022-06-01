ca8cfdf0ab3afd19fc6e4d6cd6594f76361ebbd6
Catop <camivad@gmail.com>
2022-05-31 22:56:39 -0500

    proof console

--- app.js
+++ app.js
@@ -14,8 +14,25 @@ const app = new Vue({
                nombre: this.nuevaTarea,
                estado: false
            });
-         
             this.nuevaTarea = '';
+            localStorage.setItem('encuesta-local', JSON.stringify(this.tareas));
+        },
+        editarTarea: function(index){
+            this.tareas[index].estado = true;
+            localStorage.setItem('encuesta-local', JSON.stringify(this.tareas));
+        },
+        eliminar: function(index){
+            this.tareas.splice(index, 1);
+            localStorage.setItem('encuesta-local', JSON.stringify(this.tareas));
+        }
+    },
+
+    create: function(){
+        let datosDb = JSON.parse(localStorage.getItem('encuesta-local'));
+        if(datosDb === null){
+            this.tareas = [];
+        }else{
+            this.tareas = datosDb;
         }
     }
 
